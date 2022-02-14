96ad8aba0756ea0e2a3dfe67a24c6f10eeeea415
dheemandev <dheemanchakma04@gmail.com>
2022-02-15 04:05:54 +0600

    calc

--- app.js
+++ app.js
@@ -13,3 +13,22 @@ function generatePin(){
   const pin= getPin();
   document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('calculator-pin').addEventListener('click',function(event){
+  const number = event.target.innerText;
+  const cal = document.getElementById('typed-numbers')
+
+  if(isNaN(number)){
+    console.log(number);
+    if(number == 'C'){
+      cal.value = '';
+    }
+  }
+  else{
+    const previousCal = cal.value;
+    const newCal = previousCal + number
+    cal.value = newCal;
+  
+  }
+  
+})
